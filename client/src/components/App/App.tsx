20144b5e7b35e0ca85443088fc0bf8de94a2d2e4
An Tran <an.tran@canaldigital.com>
2020-02-10 22:19:38 +0100

    Added websocket to client

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
 
 import './App.css';
 import JoinPage from '../JoinPage/JoinPage';
 
+const client = new W3CWebSocket('ws://127.0.0.1:8080');
+
 const App = () => {
+  const [userName, setUserName] = useState('');
+  const hasJoined = userName.trim().length > 0;
+
+  useEffect(() => {
+    client.onopen = () => {
+      console.log('Client connected');
+    };
+  }, []);
+
+  function handleLogin(userName: string) {
+    setUserName(userName);
+
+    client.send(
+      JSON.stringify({
+        payload: {
+          userName,
+          date: Date.now()
+        },
+        type: 'USER_JOIN'
+      })
+    );
+  }
+
   return (
     <div className="App">
       <header></header>
 
-      <JoinPage onSubmit={() => {}} />
+      <main>
+        {hasJoined ? 'Chat list here' : <JoinPage onSubmit={handleLogin} />}
+      </main>
     </div>
   );
 };
